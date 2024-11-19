5b1bf62cf1694a5bf15f663ca31989bb78613940
unknown <diogosilvabianchini@gmail.com>
2024-11-19 01:43:39 -0300

    Configuração do Postgres.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
+const {sequelize} = require("./src/database/models")
 
 const app = express()
 const PORT = process.env.PORT || 8082
 app.use(morgan("dev"))
+
+sequelize.sync({after: true}).then(() => {
+    console.log("Banco de dados conectado com sucesso!")
+})
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,20 @@
+require("dotenv").config()
+const { DB_POSTGRES_NAME, DB_POSTGRES_USER_NAME, DB_POSTGRES_PASS, DB_POSTGRES_HOST } = process.env
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": DB_POSTGRES_USER_NAME,
+    "password": DB_POSTGRES_PASS,
+    "database": DB_POSTGRES_NAME,
     "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "logging": false,
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": DB_POSTGRES_USER_NAME,
+    "password": DB_POSTGRES_PASS,
+    "database": DB_POSTGRES_NAME,
+    "host": DB_POSTGRES_HOST,
+    "dialect": "postgres"
   }
 }
